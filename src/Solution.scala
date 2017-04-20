09c79868bb2395c7b0fd6095302e3eee250d594c
Jakub Klimek <jklimek@fidolabs.com>
2017-04-20 23:37:34 +0200

    Add README

--- src/Solution.scala
+++ src/Solution.scala
@@ -5,11 +5,18 @@
 object Solution {
   def solution(s: String): Int = {
     var palindromesCount = 0
+    val strSize = s.length
+    for (i <- 1 until strSize) {
+      println(i)
+    }
 
+    println(s)
     palindromesCount
   }
 
   def main(args: Array[String]): Unit = {
-    println(solution("baababa")) // should return 6
+    var testingString = "banana"
+
+    println(solution(testingString)) // should return 4
   }
 }
