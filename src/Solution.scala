8d4f36a6ede24a2c0efaa7cdbeb1dfea07fc94ed
Jakub Klimek <jklimek@fidolabs.com>
2017-04-20 22:49:29 +0200

    Solution boilerplate

---
+++ src/Solution.scala
@@ -0,0 +1,15 @@
+/**
+  * Created by klemens on 20/04/2017.
+  */
+
+object Solution {
+  def solution(s: String): Int = {
+    var palindromesCount = 0
+
+    palindromesCount
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(solution("baababa")) // should return 6
+  }
+}
