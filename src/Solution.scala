9fa791cb360f8b579ec9b8388794967fbdf26de1
Jakub Klimek <jklimek@fidolabs.com>
2017-04-21 01:53:03 +0200

    Fix missing palindromesCount increment

--- src/Solution.scala
+++ src/Solution.scala
@@ -5,10 +5,9 @@
 object Solution {
   def solution(s: String): Int = {
     var palindromesCount = 0
-    var string = "$" + s + "%" // trick for proper iteration (string(1) == first letter)
+    val string = "$" + s + "%" // trick for intuitive iteration (string(1) == first letter)
     val origStrSize = s.length
-    val modifiedStrSize = string.length
-    var results = Array.ofDim[Int](2, origStrSize+1)
+    val results = Array.ofDim[Int](2, origStrSize+1)
 
     // Two runs, 1st for odd 2nd for even-length palindromes
     for (j <- 0 until 2) {
@@ -16,7 +15,6 @@ object Solution {
       var radius = 0
       results(j)(0) = 0
       while (i <= origStrSize) {
-        var t1 = i - radius - 1
         while (string(i - radius - 1) == string(i + j + radius)) {
           radius += 1
         }
@@ -25,6 +23,7 @@ object Solution {
         var k = 1
         while ( (results(j)(i - k) != radius - k) && (k < radius)) {
           results(j)(i + k) = math.min(results(j)(i - k), radius - k)
+          palindromesCount += results(j)(i + k)
           k += 1
         }
         radius = math.max(radius - k, 0)
@@ -37,7 +36,7 @@ object Solution {
   }
 
   def main(args: Array[String]): Unit = {
-    var testingString = "baababa"
+    var testingString = "aaabababa"
     println(solution(testingString))
   }
 }
