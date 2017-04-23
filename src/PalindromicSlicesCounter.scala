d16e6a4bd663ea3bb5178723eb731d513b2c8c84
Jakub Klimek <jklimek@fidolabs.com>
2017-04-23 23:54:55 +0200

    Add some comments Improve result printing

--- src/PalindromicSlicesCounter.scala
+++ src/PalindromicSlicesCounter.scala
@@ -3,13 +3,15 @@
   */
 
 object PalindromicSlicesCounter {
+
+  // Function counting all substring palindromes for given string s
   def solution(s: String): Int = {
     var palindromesCount = 0
     val string = "$" + s + "%" // for intuitive iteration (string(1) == first letter)
     val origStrSize = s.length
     val results = Array.ofDim[Int](2, origStrSize+1)
 
-    // Two runs, 1st for odd 2nd for even-length palindromes
+    // Two runs, 1st for even 2nd for odd-length palindromes
     for (j <- 0 until 2) {
       var i = 1
       var radius = 0
@@ -25,40 +27,42 @@ object PalindromicSlicesCounter {
         // Manacher's algorithm
         var k = 1
         while ( (results(j)(i - k) != radius - k) && (k < radius) ) {
-          results(j)(i + k) = math.min(results(j)(i - k), radius - k)
+          results(j)(i + k) = math.min(results(j)(i - k), radius - k) // Setting radius on the right hand side
           palindromesCount += results(j)(i + k)
           k += 1
         }
         radius = math.max(radius - k, 0)
-        i += k // This skipping keeps computation at O(n)
+        i += k // Skip computation for k-1 next chars
         if (palindromesCount > 100000000) return -1
       }
     }
+//    println(results.deep.mkString("\n"))
     palindromesCount
   }
 
   def main(args: Array[String]): Unit = {
     val testStringsArray = Array(
       "",
-      "a"*14142, // maximal uniform string passing (slices < 10000000) test
+      "a"*14142, // maximal uniform string passing (slices < 10000000) test (radius 7071)
       "a"*14143, // just a little too much
-      randomAlpha(20000),
-      randomAlpha(100),
+      randomLowercaseAlpha(20000),
+      randomLowercaseAlpha(20001), // string too long
+      randomLowercaseAlpha(100),
       "kobylamamalybok",
-      "abababa"
+      "r78ffffgq343qgv0bvZZZ",
+      "dabaacaabaa"
     )
-    if (testStringsArray.map(checkStringCorrectness).forall(a => a) ) {
-      testStringsArray.map(solution).foreach(println)
-    } else {
-      println("Invalid string")
-    }
+
+    testStringsArray.map(printSolution)
   }
 
-  def randomAlpha(length: Int): String = {
+  // Function generates random lowercase a-z string with given length
+  def randomLowercaseAlpha(length: Int): String = {
     val chars = 'a' to 'z'
     randomStringFromCharList(length, chars)
   }
 
+  // Function generates random string for given char sequence
   def randomStringFromCharList(length: Int, chars: Seq[Char]): String = {
     val sb = new StringBuilder
     for (i <- 1 to length) {
@@ -68,10 +72,20 @@ object PalindromicSlicesCounter {
     sb.toString
   }
 
+  // Function checks if string is suitable for the algorithm
   def checkStringCorrectness(s: String): Boolean = {
     if (s.length > 20000) return false
-    if (s.matches("[^a-z]")) return false
-
+    if (s.matches(".*[^a-z].*")) return false
     true
+  }
+
+  // Function just for printing results
+  def printSolution(s: String): Any = {
+    println("s:\t"+s)
+    if (checkStringCorrectness(s)) {
+      println("slices:\t" + solution(s) + "\n")
+    } else {
+      println("e:\tInvalid string\n")
+    }
   }
 }
